83ac8c17f122312585f1458e2e46250fcf7739eb
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2020-09-02 00:07:57 +0200

    Update ui.js

--- common-assets/ui.js
+++ common-assets/ui.js
@@ -1,11 +1,11 @@
 function autoOG(description, title, image, url) {
 
-    let og = "";
+    let og = document.getElementsByTagName("head")[0].innerHTML;
     og += "<meta property=\"og:title\" content=\"" + title + "\" - Kristóf Kékesi\"/>";
     og += "<meta property=\"og:description\" content=\"" + description + "\"/>";
     og += "<meta property=\"og:type\" content=\"my_namespace:Portfolio\"/>";
     og += "<meta property=\"og:image\" content=\"" + image + "\"/>";
     og += "<meta property=\"og:url\" content=\"" + url + "\" />";
 
-    document.getElementById("auto_og").innerHTML = og;
+    document.getElementsByTagName("head")[0].innerHTML = og;
 }
