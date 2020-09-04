97b41ad956fef60b6334f4727aab9dd2476098e4
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2020-09-04 20:34:18 +0200

    upd

--- common-assets/ui.js
+++ common-assets/ui.js
@@ -9,3 +9,20 @@ function autoOG(description, title, image, url) {
 
     document.getElementsByTagName("head")[0].innerHTML = og;
 }
+
+function autoTWITTERAPP(site, siteId, title, description, image, lang, appName, appIdAndroid, appLinkAndroid) {
+
+    let og = document.getElementsByTagName("head")[0].innerHTML;
+    og += "<meta name=\"twitter:site\" content=\"" + site +"\"/>";
+    og += "<meta name=\"twitter:site:id\" content=\"" + siteId + "\" />";
+    og += "<meta name=\"twitter:title\" content=\"" + title + " - Kristóf Kékesi\" />";
+    og += "<meta name=\"twitter:description\" content=\"" + description + "\" />";
+    og += "<meta name=\"twitter:card\" content=\"app\" />";
+    og += "<meta name=\"twitter:image:src\" content=\"" + image + "\" />";
+    og += "<meta name=\"twitter:app:country\" content=\"" + lang + "\">";
+    og += "<meta name=\"twitter:app:name:googleplay\" content=\"" + appName + "\">";
+    og += "<meta name=\"twitter:app:id:googleplay\" content=\"" + appIdAndroid + "\">";
+    og += "<meta name=\"twitter:app:url:googleplay\" content=\"" + appLinkAndroid + "\">";
+
+    document.getElementsByTagName("head")[0].innerHTML = og;
+}
