8d9727018d1902d2b2d0c28f3798c1b0b977f333
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2021-06-08 15:44:44 +0200

    create react app
    - create react app
    - dock component (for test purposes)

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Dock from "./components/dock.js";
+
+function App() {
+  return (
+    <Dock />
+  );
+}
+
+export default App;
---
+++ src/components/dock.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Dock extends Component {
+    render() {
+        return <div>Dock</div>;
+    };
+};
+
+export default Dock;
