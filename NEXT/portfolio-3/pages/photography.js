80dfc54c45714970472aeadaf74067d133063f38
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2022-06-01 23:12:23 +0200

    Update photography.js

--- NEXT/portfolio-3/pages/photography.js
+++ NEXT/portfolio-3/pages/photography.js
@@ -24,59 +24,59 @@ import cursorSetup from '../functions/cursor.js';
 
 
 export default function Home() {
-  useEffect(() => {
-    navbarToggle();
-    cursorSetup();
-  }, []);
+useEffect(() => {
+	navbarToggle();
+	cursorSetup();
+}, []);
 
-  return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
+return (
+	<>
+		<Head>
+			<meta charSet="utf-8" />
 
-        <link rel="icon" href={favicon} />
+			<link rel="icon" href={favicon} />
 
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#ffffff" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<meta name="theme-color" content="#ffffff" />
 
-        <meta name="author" content="Kristóf Kékesi" />
-        <meta name="description" content="I'm Kristóf Kékesi, developer of Elements and more." />
-        <meta name="keywords" content="HTML,CSS,JavaScript,Flutter,React,Dart,Programming,Programmer,Developer,Mobile,Web" />
+			<meta name="author" content="Kristóf Kékesi" />
+			<meta name="description" content="I'm Kristóf Kékesi, developer of Elements and more." />
+			<meta name="keywords" content="HTML,CSS,JavaScript,Flutter,React,Dart,Programming,Programmer,Developer,Mobile,Web" />
 
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@KristofKekesi" />
-        <meta name="twitter:title" content="Kristóf Kékesi" />
-        <meta name="twitter:description" content="I'm Kristóf Kékesi, developer of Elements and more." />
-        <meta name="twitter:image" content={opengraphImage}/>
+			<meta name="twitter:card" content="summary" />
+			<meta name="twitter:site" content="@KristofKekesi" />
+			<meta name="twitter:title" content="Kristóf Kékesi" />
+			<meta name="twitter:description" content="I'm Kristóf Kékesi, developer of Elements and more." />
+			<meta name="twitter:image" content={opengraphImage}/>
 
-        <meta name="og:url" content="%PUBLIC_URL%" />
-        <meta name="og:type" content="website" />
-        <meta name="og:description" content="I'm Kristóf Kékesi, developer of Elements and more." />
-        <meta name="og:image" content={opengraphImage} />
+			<meta name="og:url" content="%PUBLIC_URL%" />
+			<meta name="og:type" content="website" />
+			<meta name="og:description" content="I'm Kristóf Kékesi, developer of Elements and more." />
+			<meta name="og:image" content={opengraphImage} />
 
-        <link rel="apple-touch-icon" href={favicon} />
-        <link rel="manifest" href="manifest.json" />
+			<link rel="apple-touch-icon" href={favicon} />
+			<link rel="manifest" href="manifest.json" />
 
-        <Script async="" src="https://www.googletagmanager.com/gtag/js?id=G-NMTQ12KGY9"></Script>
+			<Script async="" src="https://www.googletagmanager.com/gtag/js?id=G-NMTQ12KGY9"></Script>
 
-        <title>Photography</title>
-      </Head>
+			<title>Photography</title>
+		</Head>
 
-        <Navbar />
+		<Navbar />
 
-        <main>
-          <center className="w-full bg-cover" style={{paddingTop: "150px", paddingBottom: "75px", backgroundImage: `url('bg.jpeg')`}}>
-          <div className="w-max">
-          <h1 className="text-white text-7xl font-bold text-left font-interBold" style={{paddingTop: "0vh", textShadow: "6px 6px 12px rgba(0, 0, 0, .5)"}}>Photography</h1>
-          </div>
-          </center>
-          <Article id="12"></Article>
-        </main>
+		<main>
+			<center className="w-full bg-cover" style={{paddingTop: "150px", paddingBottom: "75px", backgroundImage: `url('bg.jpeg')`}}>
+			<div className="w-max">
+			<h1 className="text-white text-7xl font-bold text-left font-interBold" style={{paddingTop: "0vh", textShadow: "6px 6px 12px rgba(0, 0, 0, .5)"}}>Photography</h1>
+			</div>
+			</center>
+			<Article id="12"></Article>
+		</main>
 
-        <Dock />
-        <Footer />
+		<Dock />
+		<Footer />
 
-        <Cursor />
-    </>
-  )
+		<Cursor />
+	</>
+)
 }
