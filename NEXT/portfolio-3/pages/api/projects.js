20ec7596c1323340c710bf23e5e7936b5648d651
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2022-06-04 23:53:52 +0200

    upd

---
+++ NEXT/portfolio-3/pages/api/projects.js
@@ -0,0 +1,46 @@
+import { Pool } from "pg";
+
+let conn;
+
+if (!conn) {
+	conn = new Pool({
+		user: "kristofkekesi",
+		password: "",
+		host: "localhost",
+		port: 5432,
+		database: "portfolio",
+	});
+}
+
+export default async (req, res) => {
+	try {
+		const mainQuery = 'SELECT * FROM projects';
+		const mainResult = await conn.query(mainQuery);
+
+		for (let i = 0; i < mainResult.rows.length; i++) {
+			mainResult.rows[i].skillIDs = [];
+			mainResult.rows[i].toolIDs  = [];
+
+			// Skills
+			const skillsSideQuery = "SELECT * FROM article_skills WHERE \"articleID\" = " + mainResult.rows[i].id;
+			const skillsSideResult = await conn.query(skillsSideQuery);
+			for (let j = 0; j < skillsSideResult.rows.length; j++) {
+				mainResult.rows[i].skillIDs.push(skillsSideResult.rows[j].skillID);
+			}
+
+			// Tools
+			const toolsSideQuery = "SELECT * FROM article_tools WHERE \"articleID\" = " + mainResult.rows[i].id;
+			const toolsSideResult = await conn.query(toolsSideQuery);
+			for (let j = 0; j < toolsSideResult.rows.length; j++) {
+				mainResult.rows[i].toolIDs.push(toolsSideResult.rows[j].toolID);
+			}
+
+			if (i === mainResult.rows.length - 1) {
+				return res.status(200).json(mainResult.rows);
+			}
+		}
+
+	} catch ( error ) {
+		console.log( error );
+	}
+};
