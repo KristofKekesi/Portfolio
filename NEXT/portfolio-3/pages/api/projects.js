7109596b43b94f004ba2d00ea70490eb38942d0a
Kristóf Kékesi <64812024+KristofKekesi@users.noreply.github.com>
2022-06-07 01:04:01 +0200

    upd

--- NEXT/portfolio-3/pages/api/projects.js
+++ NEXT/portfolio-3/pages/api/projects.js
@@ -32,11 +32,20 @@ export default async (req, res) => {
 		selectorQueries.push('"projects"."version" = ' + '\'' + version + '\'');
 	}
 	if (role != undefined) {
-		// search if role is in array
 		imports.push('"project_roles"');
+		selectorQueries.push('"project_roles"."role" = \'' + role + '\' AND "projects"."id" = "project_roles"."projectID"');
 	}
 	if (platform != undefined) {
-		selectorQueries.push('"projects"."platform" = ' + '\'' + platform + '\'');
+		imports.push('"project_platforms"');
+		selectorQueries.push('"project_platforms"."platform" = \'' + platform + '\' AND "projects"."id" = "project_platforms"."projectID"');
+	}
+	if (bundle != undefined) {
+		imports.push('"project_bundles"');
+		selectorQueries.push('"project_bundles"."bundleID" = \'' + bundle + '\' AND "projects"."id" = "project_bundles"."projectID"');
+	}
+	if (download != undefined) {
+		imports.push('"project_downloads"');
+		selectorQueries.push('"project_downloads"."type" = \'' + download + '\' AND "projects"."id" = "project_downloads"."projectID"');
 	}
 	if (skill != undefined) {
 		imports.push('"project_skills"');
@@ -60,8 +69,49 @@ export default async (req, res) => {
 		const mainResult = await conn.query(mainQuery);
 
 		for (let i = 0; i < mainResult.rows.length; i++) {
+			mainResult.rows[i].bundleIDs = [];
+			mainResult.rows[i].downloads = [];
+			mainResult.rows[i].platforms = [];
+			mainResult.rows[i].roles = [];
+			mainResult.rows[i].screenshots = [];
 			mainResult.rows[i].skillIDs = [];
 			mainResult.rows[i].toolIDs  = [];
+
+			// Project Bundles
+			const bundlesSideQuery = "SELECT * FROM project_bundles WHERE \"projectID\" = " + mainResult.rows[i].id;
+			const bundlesSideResult = await conn.query(bundlesSideQuery);
+			for (let j = 0; j < bundlesSideResult.rows.length; j++) {
+				mainResult.rows[i].bundleIDs.push(bundlesSideResult.rows[j].bundleID);
+			}
+
+			// Downloads
+			const downloadsSideQuery = "SELECT * FROM project_downloads WHERE \"projectID\" = " + mainResult.rows[i].id;
+			const downloadsSideResult = await conn.query(downloadsSideQuery);
+			for (let j = 0; j < downloadsSideResult.rows.length; j++) {
+				delete downloadsSideResult.rows[j].projectID;
+				mainResult.rows[i].downloads.push(downloadsSideResult.rows[j]);
+			}
+
+			// Platforms
+			const platformsSideQuery = "SELECT * FROM project_platforms WHERE \"projectID\" = " + mainResult.rows[i].id;
+			const platformsSideResult = await conn.query(platformsSideQuery);
+			for (let j = 0; j < platformsSideResult.rows.length; j++) {
+				mainResult.rows[i].platforms.push(platformsSideResult.rows[j].platform);
+			}
+
+			// Roles
+			const rolesSideQuery = "SELECT * FROM project_roles WHERE \"projectID\" = " + mainResult.rows[i].id;
+			const rolesSideResult = await conn.query(rolesSideQuery);
+			for (let j = 0; j < rolesSideResult.rows.length; j++) {
+				mainResult.rows[i].roles.push(rolesSideResult.rows[j].role);
+			}
+
+			// Screenshots
+			const screenshotsSideQuery = "SELECT * FROM project_screenshots WHERE \"projectID\" = " + mainResult.rows[i].id;
+			const screenshotsSideResult = await conn.query(screenshotsSideQuery);
+			for (let j = 0; j < screenshotsSideResult.rows.length; j++) {
+				mainResult.rows[i].screenshots.push(screenshotsSideResult.rows[j].imageID);
+			}
 
 			// Skills
 			const skillsSideQuery = "SELECT * FROM project_skills WHERE \"projectID\" = " + mainResult.rows[i].id;
